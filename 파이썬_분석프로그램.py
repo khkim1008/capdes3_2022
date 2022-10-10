4d37e5d1405d285072bdd2cb74de2efe1ac5fae5
khkim1008 <khkim@jejunu.ac.kr>
2022-10-11 07:24:08 +0900

    10월11일 업로드

--- 파이썬_분석프로그램.py
+++ 파이썬_분석프로그램.py
@@ -1,49 +1,71 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct  6 17:49:59 2022
-
-@author: User
-"""
-
 import numpy as np
 import pandas as pd
 
 
 from matplotlib import font_manager, rc
-font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
-rc('font', family=font_name)
 
 
-a = np.arange(1,6)
-b = pd.date_range("2019-09-01", periods=5, freq="D")
-s = pd.Series(a,index=b)
-s
 dataset = pd.read_excel('D:/2022-2학기/캡스톤디자인/data/BUS_02_이용자유형별버스정류소이용인원현황.xlsx') 
 dataset.info()
-dataset['base_date'][0]
 
-dataset['base_date'] = dataset['base_date'].astype(str)
+#날짜(정수형)을 문자열로 변환
+dataset['base_date'] = dataset['base_date'].astype(str)  
 
-temp = dataset.groupby('base_date').count()
+#날짜별로 그룹핑하여 탑승객 수 집계 
+temp = dataset.groupby('base_date').count() 
 temp.info()
 
 temp['탑승객수'] = temp['user_count']
-temp.to_excel('D:/2022-2학기/캡스톤디자인/data/bus2019년날짜별탑승객수.xlsx')   
+
+ #날짜별로 그룹핑하여 탑승객 수 집계 데이터 화일로 저장
+temp.to_excel('D:/2022-2학기/캡스톤디자인/data/bus2019년날짜별탑승객수.xlsx')  
+
+ #날짜별로  탑승객 수 선그래프 출력
 temp.plot( y=['탑승객수'])
 
+ #사용자 유형별로 그룹핑하여 탑승객 수 집계 
 temp_user = dataset.groupby('user_type').count()
-temp_user_sort = temp_user.sort_values(by='user_count', ascending=False)
-temp_user.to_excel('D:/2022-2학기/캡스톤디자인/data/bus2019년사용자별탑승객수.xlsx')   
 
+ #탑승객 수 별 내림차순 정렬
+temp_user_sort = temp_user.sort_values(by='user_count', ascending=False) 
+
+ #사용자 유형별로 그룹핑하여 탑승객 수 집계 데이터 화일로 저장
+temp_user.to_excel('D:/2022-2학기/캡스톤디자인/data/bus2019년사용자별탑승객수.xlsx')   
+temp_user['탑승객수'] = temp_user['user_count']
+
+#한글 출력 깨짐 방지
+font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+rc('font', family=font_name)
+
+ #사용자 유형별로  탑승객 수를 선그래프 출력
+
+temp_user.plot( y=['탑승객수'])
+
+temp_user_sort['탑승객수'] = temp_user_sort['user_count']
+temp_user_sort.plot( y=['탑승객수'])
+
+ #정류소 별로 그룹핑하여 탑승객 수 집계 
+ 
 temp_sn = dataset.groupby('station_name').count()
+
+ #정류소별 탑승객 수 별 내림차순 정렬
+temp_sn_sort = temp_sn.sort_values(by='user_count', ascending=False) 
+
+ #정류소 별로  탑승객 수를 선그래프 출력
+temp_sn_sort['탑승객수'] = temp_sn_sort['user_count']
+temp_sn_sort.plot( y=['탑승객수'])
+
 temp_sn.to_excel('D:/2022-2학기/캡스톤디자인/data/bus2019년정류소별탑승객수.xlsx')   
 
+#시계열 데이터 변환 및 처리 -------------------------------------
 
-dataset['base_date'] = pd.to_datetime(dataset['base_date'] )
-dataset.set_index('base_date', inplace=True)
-dataset.info()
+tdf = pd.read_excel('D:/2022-2학기/캡스톤디자인/data/bus2019년날짜별탑승객수.xlsx') 
 
-dataset.head(5)
+tdf['base_date'] = pd.to_datetime(tdf['base_date'] )
+tdf.set_index('base_date', inplace=True)
+tdf.info()
+
+tdf.head(5)
 
 dataset.to_excel('D:/2022-2학기/캡스톤디자인/data/bus2019.xlsx')   
 
@@ -58,6 +80,10 @@ df.set_index('일자', inplace=True)
 
 
 #df.plot( y=['탑승객수'])
+
+font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+rc('font', family=font_name)
+
 
 df.plot.line( y=['탑승객수'])
 #df.plot.bar( y=['탑승객수'])
